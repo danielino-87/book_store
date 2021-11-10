35779bb248a7f38ebc43160c50b5334521f8879e
danielino-87 <danieleiovinelli@gmail.com>
2021-11-10 19:59:04 +0100

    Create Gitignore File

--- bookoutlet/models.py
+++ bookoutlet/models.py
@@ -10,16 +10,12 @@ class Book (models.Model):
            validators =[ MinValueValidator(1) , MaxValueValidator(5)])
     author = models.CharField(null=True , max_length=100) 
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="",null=False, db_index=True) 
+    slug = models.SlugField(default="",blank=True ,null=False, db_index=True) 
 
     def get_absolute_url(self):
         return reverse ("book-detail" , args=[self.slug])
 
 
-    def save(self , *args,**kwards ):
-
-        self.slug = slugify(self.title)
-        super().save(*args,**kwards)
     
 
     def __str__(self):
