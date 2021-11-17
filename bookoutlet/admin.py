711b1d628b472288e155385632e118d4ee51ea31
danielino-87 <danieleiovinelli@gmail.com>
2021-11-17 17:24:56 +0100

    Create Gitignore File

--- bookoutlet/admin.py
+++ bookoutlet/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book , Author
 
 # Register your models here.
+
+
 
 class BookAdmin(admin.ModelAdmin):
    
@@ -11,3 +13,4 @@ class BookAdmin(admin.ModelAdmin):
    list_display = ("title" , "author" , )
 
 admin.site.register(Book , BookAdmin)
+admin.site.register(Author)
