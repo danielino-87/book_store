6444481c34d963bb2fc856a73ad9595e195feacf
danielino-87 <danieleiovinelli@gmail.com>
2021-11-22 15:05:04 +0100

    Create Gitignore File

--- bookoutlet/models.py
+++ bookoutlet/models.py
@@ -9,6 +9,14 @@ class Country(models.Model):
     name = models.CharField(max_length=80)
     code = models.CharField(max_length=2)
 
+    def __str__(self):
+        return f"{self.name}  ({self.code}) "
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
+    
+
 class Address(models.Model):
     street = models.CharField(max_length=80)
     postal_code = models.CharField(max_length=5)
@@ -49,7 +57,7 @@ class Book (models.Model):
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="",blank=True ,null=False, db_index=True) 
     
-    published_countries = models.ManyToManyField(Country)
+    published_countries = models.ManyToManyField(Country , null=False )
     def get_absolute_url(self):
         return reverse ("book-detail" , args=[self.slug])
 
